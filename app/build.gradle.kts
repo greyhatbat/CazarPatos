9b1282c4893921da84b7eb74ca3ff13c928fc6f4
greyhatbat <agzumn@gmail.com>
2026-01-24 16:50:51 -0500

    feat: Implement player ranking with SQLite database
    - Add `RankingPlayerDBHelper` to manage a SQLite database for storing player scores.
    - Implement CRUD (Create, Read, Update, Delete) operations for the player ranking data.
    - Introduce `RankingActivity` to display the player rankings in a `RecyclerView`.
    - Create `RankingAdapter` to populate the `RecyclerView` with sorted player data.
    - Add `Player` data class to model player information (username, huntedDucks).
    - Define new layouts (`activity_ranking.xml`, `ranking_list.xml`) and string resources for the ranking feature.
    - Set `RankingActivity` as the new launcher activity in `AndroidManifest.xml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -37,12 +38,16 @@ android {
 
 dependencies {
 
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.security.crypto)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/zumbana/alvaro/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/zumbana/alvaro/cazarpatos/LoginActivity.kt
@@ -3,11 +3,16 @@ package com.zumbana.alvaro.cazarpatos
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class LoginActivity : AppCompatActivity() {
@@ -18,6 +23,8 @@ class LoginActivity : AppCompatActivity() {
     lateinit var buttonNewUser:Button
     lateinit var checkBoxRecordarme: CheckBox
     lateinit var mediaPlayer: MediaPlayer
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -35,6 +42,10 @@ class LoginActivity : AppCompatActivity() {
         buttonNewUser = findViewById(R.id.buttonNewUser)
         checkBoxRecordarme = findViewById(R.id.checkBoxRecordarme)
 
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
         LeerDatosDePreferencias()
 
         //Eventos clic
@@ -47,10 +58,11 @@ class LoginActivity : AppCompatActivity() {
             //Guardar datos en preferencias.
             GuardarDatosEnPreferencias()
             //Si pasa validación de datos requeridos, ir a pantalla principal
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra(EXTRA_LOGIN, email)
-            startActivity(intent)
-            finish()
+//            val intent = Intent(this, MainActivity::class.java)
+//            intent.putExtra(EXTRA_LOGIN, email)
+//            startActivity(intent)
+//            finish()
+            AutenticarUsuario(email, clave)
         }
         buttonNewUser.setOnClickListener{
 
@@ -58,6 +70,25 @@ class LoginActivity : AppCompatActivity() {
         mediaPlayer=MediaPlayer.create(this, R.raw.title_screen)
         mediaPlayer.start()
     }
+
+    fun AutenticarUsuario(email:String, password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(EXTRA_LOGIN, "signInWithEmail:success")
+                    //Si pasa validación de datos requeridos, ir a pantalla principal
+                    val intencion = Intent(this, MainActivity::class.java)
+                    intencion.putExtra(EXTRA_LOGIN, auth.currentUser!!.email)
+                    startActivity(intencion)
+                    //finish()
+                } else {
+                    Log.w(EXTRA_LOGIN, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, task.exception!!.message,
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 
     private fun GuardarDatosEnPreferencias(){
         val email = editTextEmail.text.toString()
